47dd97096d37d8d2bf2be8ec68caff87f1c8a06c
aleexrodriguez93@gmail.com <aleexrodriguez93@gmail.com>
2022-10-09 02:43:22 -0300

    chnage a variable name

--- js/modules/products.js
+++ js/modules/products.js
@@ -2,14 +2,14 @@
 
 import {groupFunctions} from './products-functions.js';
 
-let hola=async()=>{
+let products=async()=>{
     let url="./js/modules/products.json";
     let productsDate=await fetch(url);
     let products=await productsDate.json();
     return products
 }
 
-groupFunctions(hola)
+groupFunctions(products)
 
 
 
